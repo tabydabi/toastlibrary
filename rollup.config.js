c8e55f470456db5667858ca56657889dfc73aa3a
Kirill Borun <69752848+tabydabi@users.noreply.github.com>
2021-09-22 10:06:22 +0300

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import generatePackageJson from "rollup-plugin-generate-package-json";
-import svg from "rollup-plugin-svg";
-import svgr from "@svgr/rollup";
-import url from "rollup-plugin-url";
+import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
+import commonjs from "rollup-plugin-commonjs"
+import generatePackageJson from "rollup-plugin-generate-package-json"
+import svg from "rollup-plugin-svg"
+import svgr from "@svgr/rollup"
+import url from "rollup-plugin-url"
 
 export default {
   input: "src/index.jsx",
   output: {
     file: "dist/bundles/bundle.js",
-    format: "cjs",
+    format: "iife",
   },
   external: ["react", "react-dom", "styled-components"],
   plugins: [
@@ -34,4 +34,4 @@ export default {
       }),
     }),
   ],
-};
+}
